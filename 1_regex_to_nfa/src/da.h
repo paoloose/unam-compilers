bdd1e577939f1f4f5328196972cbaf2fab524922
Paolo Flores <pflores.fisi22@gmail.com>
2026-03-06 01:59:52 -0600

    update dynamic array utils

--- 1_regex_to_nfa/src/da.h
+++ 1_regex_to_nfa/src/da.h
@@ -14,9 +14,6 @@ typedef struct { \
 } da_##NAME; \
 \
 static inline void da_##NAME##_init(da_##NAME* da, int initial_capacity) { \
-    if (da->data) { \
-        free(da->data); \
-    } \
     da->data = (TYPE*)calloc(initial_capacity, sizeof(TYPE)); \
     da->capacity = initial_capacity; \
     da->length = 0; \
@@ -27,7 +24,7 @@ static inline void da_##NAME##_append(da_##NAME* da, TYPE item) { \
         da_##NAME##_init(da, DA_INITIAL_CAPACITY); \
     } \
     \
-    if (da->capacity == da->length) { \
+    if (da->capacity <= da->length) { \
         TYPE* old_data = da->data; \
         TYPE* new_data = (TYPE*)calloc(da->capacity * 2, sizeof(TYPE)); \
         memcpy(new_data, old_data, sizeof(TYPE) * da->length); \
@@ -39,7 +36,7 @@ static inline void da_##NAME##_append(da_##NAME* da, TYPE item) { \
     da->data[da->length++] = item; \
 } \
 \
-static inline TYPE* da_##NAME##_peek_last(da_##NAME* da) { \
+static inline TYPE* da_##NAME##_peek_last(const da_##NAME* da) { \
     if (!da->data || da->length == 0) return NULL; \
     return &da->data[da->length - 1]; \
 } \
@@ -48,16 +45,21 @@ static inline TYPE* da_##NAME##_pop(da_##NAME* da) { \
     if (!da->data || da->length == 0) return NULL; \
     da->length--; \
     return &da->data[da->length]; \
-} \
-\
-static inline void da_##NAME##_free(da_##NAME* da) { \
-    if (da->data) { \
-        free(da->data); \
-        da->data = NULL; \
-        da->capacity = 0; \
-        da->length = 0; \
-    } \
 }
+
+#define da_free(__DA) \
+    { \
+        if ((__DA)->data) { \
+            free((__DA)->data); \
+            (__DA)->data = NULL; \
+            (__DA)->capacity = 0; \
+            (__DA)->length = 0; \
+        } \
+    }
+
+#define da_empty(__DA) ((__DA)->length == 0)
+
+#define da_drain(__DA) {(__DA)->length = 0;}
 
 // Generate implementations for common types
 create_da(int, int)
@@ -65,3 +67,7 @@ create_da(float, float)
 create_da(double, double)
 create_da(char, char)
 create_da(nfa_fragment, nfa_fragment)
+create_da(nfa_node*, nfa_node)
+create_da(input_op, input_op)
+create_da(regex_item, regex_item)
+create_da(void*, pointer)
