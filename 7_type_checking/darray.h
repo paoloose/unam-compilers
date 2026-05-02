4f7a2cf553ff750b3f1deac48f7dc9a2de512bb9
Paolo Flores <pflores.fisi22@gmail.com>
2026-05-02 11:46:39 -0600

    add dynamic array and debug utils

---
+++ 7_type_checking/darray.h
@@ -0,0 +1,72 @@
+#pragma once
+#include <stdlib.h>
+#include <string.h>
+
+#define DA_INITIAL_CAPACITY 8
+
+#define create_da(TYPE, NAME) \
+typedef struct { \
+    int capacity; \
+    int length; \
+    TYPE* data; \
+} da_##NAME; \
+\
+static inline void da_##NAME##_init(da_##NAME* da, int initial_capacity) { \
+    da->data = (TYPE*)calloc(initial_capacity, sizeof(TYPE)); \
+    da->capacity = initial_capacity; \
+    da->length = 0; \
+} \
+\
+static inline void da_##NAME##_append(da_##NAME* da, TYPE item) { \
+    if (!da->data) { \
+        da_##NAME##_init(da, DA_INITIAL_CAPACITY); \
+    } \
+    \
+    if (da->capacity <= da->length) { \
+        TYPE* old_data = da->data; \
+        TYPE* new_data = (TYPE*)calloc(da->capacity * 2, sizeof(TYPE)); \
+        memcpy(new_data, old_data, sizeof(TYPE) * da->length); \
+        free(old_data); \
+        da->capacity *= 2; \
+        da->data = new_data; \
+    } \
+    \
+    da->data[da->length++] = item; \
+} \
+\
+static inline TYPE* da_##NAME##_peek_last(const da_##NAME* da) { \
+    if (!da->data || da->length == 0) return NULL; \
+    return &da->data[da->length - 1]; \
+} \
+\
+static inline TYPE* da_##NAME##_pop(da_##NAME* da) { \
+    if (!da->data || da->length == 0) return NULL; \
+    da->length--; \
+    return &da->data[da->length]; \
+}
+
+#define da_free(__DA) \
+    { \
+        if ((__DA)->data) { \
+            free((__DA)->data); \
+            (__DA)->data = NULL; \
+            (__DA)->capacity = 0; \
+            (__DA)->length = 0; \
+        } \
+    }
+
+#define da_empty(__DA) ((__DA)->length == 0)
+
+#define da_drain(__DA) {(__DA)->length = 0;}
+
+typedef struct __pair {
+
+} __pair;
+
+// Generate implementations for common types
+create_da(int, int)
+create_da(float, float)
+create_da(double, double)
+create_da(char*, cstr)
+create_da(char, char)
+create_da(void*, pointer)
--- 7_type_checking/debug.h
+++ 7_type_checking/debug.h
@@ -13,6 +13,15 @@
 // Returns the filename only
 #define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
 
+// Column macro for portability
+#ifndef __COLUMN__
+#   if defined(__clang__)
+#       define __COLUMN__ __builtin_COLUMN()
+#   else
+#       define __COLUMN__ 0
+#   endif
+#endif
+
 // Environment
 #ifndef UNAM_IS_DEBUG
     #define UNAM_IS_DEBUG 1
@@ -22,12 +31,16 @@
 #   define UNAM_DEBUG(x, ...) \
         fprintf( \
             stderr, \
-            UNAM_CYAN "%s:" UNAM_BLUE "%d(" UNAM_CYAN "%s" UNAM_BLUE "): " UNAM_RESET x, \
+            UNAM_CYAN "%s:" UNAM_BLUE "%d:%d(" UNAM_CYAN "%s" UNAM_BLUE "): " UNAM_RESET x, \
             __FILENAME__, \
             __LINE__, \
+            __COLUMN__, \
             __func__, \
             ##__VA_ARGS__ \
         );
+
+    #define UNAM_DEBUG_PLAIN(x, ...) \
+        fprintf(stderr, x, ##__VA_ARGS__);
 
 #   define UNAM_ASSERT(cond, msg, ...) \
         do { \
@@ -39,6 +52,7 @@
 #else
 #   define UNAM_DEBUG(x, ...) ((void)0)
 #   define UNAM_ASSERT(cond, ...) ((void)0)
+#   define UNAM_DEBUG_PLAIN(x, ...) ((void)0)
 #endif
 
 #if UNAM_IS_DEBUG == 1
