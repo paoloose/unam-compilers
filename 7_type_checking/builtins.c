dab9e028f21bd4a3fc13094f1fe8ff4f129e7220
Paolo Flores <pflores.fisi22@gmail.com>
2026-05-03 04:29:54 -0600

    fix global references were mutable, so copy instead

--- 7_type_checking/builtins.c
+++ 7_type_checking/builtins.c
@@ -1,58 +1,26 @@
-#pragma once
-
 #include "analyzer.h"
 
-ASTNode int_astnode = {
-    .type = NODE_TYPE_IDENTIFIER,
-    .lexeme = "int",
-};
+SymbolTableEntry* make_builtin(const char* name, NodeType type) {
+    ASTNode* node = calloc(1, sizeof *node);
+    if (!node) return NULL;
 
-SymbolTableEntry int_symbol = {
-    .name = "int",
-    .type_node = &int_astnode,
-    .next = NULL,
-};
+    SymbolTableEntry* sym = calloc(1, sizeof *sym);
+    if (!sym) {
+        free(node);
+        return NULL;
+    }
 
-ASTNode float_astnode = {
-    .type = NODE_TYPE_IDENTIFIER,
-    .lexeme = "float",
-};
+    node->type = type;
+    node->lexeme = name;
 
-SymbolTableEntry float_symbol = {
-    .name = "float",
-    .type_node = &float_astnode,
-    .next = NULL,
-};
+    sym->name = name;
+    sym->type_node = node;
 
-ASTNode bool_astnode = {
-    .type = NODE_TYPE_IDENTIFIER,
-    .lexeme = "bool",
-};
+    return sym;
+}
 
-SymbolTableEntry bool_symbol = {
-    .name = "bool",
-    .type_node = &bool_astnode,
-    .next = NULL,
-};
-
-ASTNode string_astnode = {
-    .type = NODE_TYPE_IDENTIFIER,
-    .lexeme = "string",
-};
-
-SymbolTableEntry string_symbol = {
-    .name = "string",
-    .type_node = &string_astnode,
-    .next = NULL,
-};
-
-ASTNode list_astnode = {
-    .type = NODE_TYPE_IDENTIFIER,
-    .lexeme = "List",
-};
-
-SymbolTableEntry list_symbol = {
-    .name = "List",
-    .type_node = &list_astnode,
-    .next = NULL,
-};
+SymbolTableEntry* get_int_symbol()    { return make_builtin("int", NODE_CONCRETE_TYPE); }
+SymbolTableEntry* get_float_symbol()  { return make_builtin("float", NODE_CONCRETE_TYPE); }
+SymbolTableEntry* get_bool_symbol()   { return make_builtin("bool", NODE_CONCRETE_TYPE); }
+SymbolTableEntry* get_string_symbol() { return make_builtin("string", NODE_CONCRETE_TYPE); }
+SymbolTableEntry* get_list_symbol()   { return make_builtin("List", NODE_GENERIC_TYPE); }
