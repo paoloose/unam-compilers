2bd117c56d1d93e6b3bb6fee59cd48522a4f8f93
Paolo Flores <pflores.fisi22@gmail.com>
2026-05-05 00:57:35 -0600

    add more colors for debugging

--- 7_type_checking/debug.h
+++ 7_type_checking/debug.h
@@ -3,12 +3,24 @@
 #include <string.h>
 
 #define UNAM_RESET   "\x1b[0m"
-#define UNAM_RED     "\x1b[31m"
-#define UNAM_MAGENTA "\x1b[35m"
-#define UNAM_BLUE    "\x1b[34m"
-#define UNAM_CYAN    "\x1b[36m"
-#define UNAM_BRIGHT_RED   "\x1b[91m"
-#define UNAM_BRIGHT_GREEN "\x1b[92m"
+
+#define UNAM_BLACK        "\x1b[30m"
+#define UNAM_RED          "\x1b[31m"
+#define UNAM_GREEN        "\x1b[32m"
+#define UNAM_YELLOW       "\x1b[33m"
+#define UNAM_BLUE         "\x1b[34m"
+#define UNAM_MAGENTA      "\x1b[35m"
+#define UNAM_CYAN         "\x1b[36m"
+#define UNAM_WHITE        "\x1b[37m"
+
+#define UNAM_BG_BLACK      "\x1b[40m"
+#define UNAM_BG_RED        "\x1b[41m"
+#define UNAM_BG_GREEN      "\x1b[42m"
+#define UNAM_BG_YELLOW     "\x1b[43m"
+#define UNAM_BG_BLUE       "\x1b[44m"
+#define UNAM_BG_MAGENTA    "\x1b[45m"
+#define UNAM_BG_CYAN       "\x1b[46m"
+#define UNAM_BG_WHITE      "\x1b[47m"
 
 // Returns the filename only
 #define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
